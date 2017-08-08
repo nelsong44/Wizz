0765143749f217febc56cf8bcb9ff0f48e02ba8e
nelsong44 <nelsong44@gmail.com>
2017-08-07 20:18:04 -0500

    formatted basic bootstrap form to capture user input for adding a new restroom to the db

--- server/public/scripts/controllers/edit.controller.js
+++ server/public/scripts/controllers/edit.controller.js
@@ -1,7 +1,14 @@
 //EditController
 
 app.controller('EditController', function($http) {
-    //attach controller to the view model
-    console.log('in EditController');
-    var edit = this;
+  console.log('in EditController');
+  //attach controller to the view model
+  var edit = this;
+  edit.features = ['Unisex', 'Handicap Accessible', 'Changing Station'];
+  edit.states = ['AK','AL','AR','AZ','CA','CO','CT','DC','DE','FL','GA','GU','HI','IA','ID', 'IL','IN','KS','KY','LA','MA','MD','ME','MI','MN','MO','MS','MT','NC','ND','NE','NH','NJ','NM','NV','NY', 'OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VA','VI','VT','WA','WI','WV','WY'];
+  edit.searchTerm;
+  edit.clearSearchTerm = function() {
+    edit.searchTerm = '';
+  };
+
 });//end EditController
